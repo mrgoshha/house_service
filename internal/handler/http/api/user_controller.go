3f1c98613e4e00ff0f56af1e53e9931f69a65910
margo <margaritabuthuzi@gmail.com>
2024-12-05 12:23:15 +0300

    improve logging

--- internal/handler/http/api/user_controller.go
+++ internal/handler/http/api/user_controller.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"github.com/gorilla/mux"
 	httpApi "houseService/internal/handler/http"
+	"houseService/internal/handler/http/middlewre"
 	apimodel "houseService/internal/handler/http/model"
 	"houseService/internal/service"
 	"log/slog"
@@ -27,15 +28,23 @@ func NewUserController(log *slog.Logger, service service.User, router *mux.Route
 }
 
 func (u *UserController) LoginPost(w http.ResponseWriter, r *http.Request) {
+	const op = "handler.http.api.LoginPost"
+
+	log := u.log.With(
+		slog.String("op", op),
+		slog.String("request_id", r.Context().Value(middlewre.CtxKeyRequestID).(string)),
+	)
 	req := &apimodel.LoginBody{}
 
 	if err := json.NewDecoder(r.Body).Decode(req); err != nil {
+		log.Error("failed to decode request")
 		ErrorResponseWithCode(w, r, http.StatusBadRequest, err)
 		return
 	}
 
 	token, err := u.service.SingIn(httpApi.ToUserLoginServiceModel(req))
 	if err != nil {
+		log.Error("failed to login user", slog.String("error", err.Error()))
 		ErrorResponse(w, r, err)
 		return
 	}
@@ -46,24 +55,35 @@ func (u *UserController) LoginPost(w http.ResponseWriter, r *http.Request) {
 }
 
 func (u *UserController) RegisterPost(w http.ResponseWriter, r *http.Request) {
+	const op = "handler.http.api.RegisterPost"
+
+	log := u.log.With(
+		slog.String("op", op),
+		slog.String("request_id", r.Context().Value(middlewre.CtxKeyRequestID).(string)),
+	)
 	req := &apimodel.RegisterBody{}
 
 	if err := json.NewDecoder(r.Body).Decode(req); err != nil {
+		log.Error("failed to decode request")
 		ErrorResponseWithCode(w, r, http.StatusBadRequest, err)
 		return
 	}
 
 	userServiceModel, err := httpApi.ToUserServiceModel(req)
 	if err != nil {
+		log.Error("failed to decode request")
 		ErrorResponseWithCode(w, r, http.StatusBadRequest, err)
 		return
 	}
 
 	uuid, err := u.service.SingUp(userServiceModel)
 	if err != nil {
+		log.Error("failed to register user", slog.String("error", err.Error()))
 		ErrorResponse(w, r, err)
 		return
 	}
+
+	log.Info("create new user")
 
 	res := httpApi.ToUUIDApiModel(uuid)
 
